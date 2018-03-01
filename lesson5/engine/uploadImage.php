956dc71a3612cf3025a725177909f17968a8c502
Filipp <fil106@mail.ru>
2018-03-01 17:43:14 +0300

    ДЗ 5

--- lesson5/engine/uploadImage.php
+++ lesson5/engine/uploadImage.php
@@ -1,5 +1,9 @@
 <?php
 
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+
 	require_once('../config/config.php');
 	require_once('../engine/db/functions.php');
 	require_once('resize.php');
